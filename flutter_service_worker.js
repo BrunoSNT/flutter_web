6d7278c49fde1a60a8d8a4cd41f1ec9bd308293f
Bruno Barros <brunoebarros@gmail.com>
2020-05-17 10:31:07 -0300

    scaffold init

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "28ce2d05f8cae689fee5c512ce989bdd",
 "/": "28ce2d05f8cae689fee5c512ce989bdd",
-"main.dart.js": "b7803568570477162676755894d22c6c",
+"main.dart.js": "cb309f50e4ea560d210e163bf4e087c8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "b098d55a747e68376dbec693dc2c4a43",
-"assets/LICENSE": "c82db968186b51e2e6a72ce0491edf4c",
+"assets/LICENSE": "99d3854f57414739d4afeffdab8f8de3",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
